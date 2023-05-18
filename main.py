ab2454535acde38ae1d313ab5ed150b443a565c1
geniuskpj <geniuskpj@gmail.com>
2023-05-18 06:35:41 +0000

    capture edited

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import js
-from js import Option,console,getPixels,document
+from js import Option,console,document
 from pyodide.ffi import create_proxy
 import asyncio
 def capture(e):
@@ -7,7 +7,7 @@ def capture(e):
     file_list = e.target.files
     first_item = file_list.item(0)
     document.getElementById('output').appendChild(first_item)
-    return getPixels(first_item)
+    return js.getPixels(first_item)
 
 document.getElementById('cam').addEventListener('change',capture)
 
